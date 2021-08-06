9758184b7ecd063ff37d8d4a2a3df057e6791b43
heart-hacker786 <abdul.wahab.tanveer009@gmail.com>
2021-08-06 18:37:38 +0500

    Added First Project

--- My_Andriod_Projects/UiBasics/app/src/main/java/com/org/uibasics/MainActivity.java
+++ My_Andriod_Projects/UiBasics/app/src/main/java/com/org/uibasics/MainActivity.java
@@ -3,7 +3,6 @@ package com.org.uibasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,15 +16,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoginbtnClicked(View view){
-        EditText email = findViewById(R.id.Email);
+        EditText name = findViewById(R.id.Name);
+
+        setContentView(R.layout.activity_login);
 
         TextView t1= findViewById(R.id.Result);
-        t1.setText("Email: "+email.getText().toString());
+        t1.setText("Name: "+name.getText().toString());
 
-        EditText password = findViewById(R.id.Pass);
+//        EditText password = findViewById(R.id.Pass);
 
 
         TextView t2= findViewById(R.id.Result2nd);
-        t2.setText("Password: "+ "Maa nai Batoun ga");
+        t2.setText("Password: "+ "Maa Nai Batoun Ga ;)");
+
+        TextView t3= findViewById(R.id.Greeting);
+        t3.setText("Hi!, "+ name.getText().toString() +" We are very happy that you are here and we will be" +
+                " Pleased to see you again and again soon \nThanks for coming");
+
+
     }
 }
